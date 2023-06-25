0abce58fa02929915a604087d546742023981e74
eloylp <eloylp@eloylp.dev>
2023-06-25 21:44:03 +0200

    Add PRD creation and query methods

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 #![no_std]
 
-use soroban_sdk::{contracterror, contractimpl, contracttype, Address, ConversionError, Env, Vec};
+use soroban_sdk::{
+    contracterror, contractimpl, contracttype, Address, ConversionError, Env, Map, Vec,
+};
 
 #[contracterror]
 #[derive(Clone, Debug, Copy, Eq, PartialEq, PartialOrd, Ord)]
@@ -8,6 +10,7 @@ use soroban_sdk::{contracterror, contractimpl, contracttype, Address, Conversion
 pub enum Error {
     Conversion = 1,
     KeyExpected = 2,
+    NotFound = 3,
 }
 
 impl From<ConversionError> for Error {
@@ -17,10 +20,20 @@ impl From<ConversionError> for Error {
 }
 
 #[contracttype]
+#[derive(Clone, Debug, Copy, Eq, PartialEq, PartialOrd, Ord)]
+#[repr(u32)]
+pub enum Status {
+    OpenVoting = 1,
+    Approved = 2,
+    Rejected = 3,
+}
+
+#[contracttype]
 #[derive(Clone, Copy)]
 pub enum DataKey {
     VoterList = 0,
     Admin = 1,
+    PRDStorage = 2,
 }
 
 pub struct ProposalContract;
@@ -30,7 +43,11 @@ impl ProposalContract {
     pub fn init(env: Env, admin: Address) {
         env.storage().set(&DataKey::Admin, &admin);
         env.storage()
-            .set(&DataKey::VoterList, &Vec::<Address>::new(&env))
+            .set(&DataKey::VoterList, &Vec::<Address>::new(&env));
+        env.storage().set(
+            &DataKey::PRDStorage,
+            &Map::<u64, (Status, i64, Vec<Address>)>::new(&env),
+        )
     }
 
     pub fn add_voters(env: Env, voters: Vec<Address>) -> Result<(), Error> {
@@ -46,7 +63,8 @@ impl ProposalContract {
 
         voter_list.append(&voters);
 
-        Ok(env.storage().set(&DataKey::VoterList, &voter_list))
+        env.storage().set(&DataKey::VoterList, &voter_list);
+        Ok(())
     }
 
     pub fn get_voters(env: Env) -> Result<Vec<Address>, Error> {
@@ -54,6 +72,45 @@ impl ProposalContract {
             .get(&DataKey::VoterList)
             .ok_or(Error::KeyExpected)??
     }
+
+    pub fn create_prd(env: Env, id: u64) -> Result<(), Error> {
+        env.storage()
+            .get::<_, Address>(&DataKey::Admin)
+            .ok_or(Error::KeyExpected)??
+            .require_auth();
+
+        let mut storage = env
+            .storage()
+            .get::<_, Map<u64, ProposalState>>(&DataKey::PRDStorage)
+            .ok_or(Error::KeyExpected)??;
+
+        storage.set(
+            id,
+            ProposalState {
+                status: Status::OpenVoting,
+                votes: 0,
+            },
+        );
+
+        env.storage().set(&DataKey::PRDStorage, &storage);
+        Ok(())
+    }
+
+    pub fn prd_status(env: Env, id: u64) -> Result<ProposalState, Error> {
+        Ok(env
+            .storage()
+            .get::<_, Map<u64, ProposalState>>(&DataKey::PRDStorage)
+            .ok_or(Error::KeyExpected)??
+            .get(id)
+            .ok_or(Error::NotFound)??)
+    }
+}
+
+#[contracttype]
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct ProposalState {
+    status: Status,
+    votes: i64,
 }
 
 #[cfg(test)]
--- src/test.rs
+++ src/test.rs
@@ -1,7 +1,7 @@
 #![cfg(test)]
 
-use crate::{ProposalContract, ProposalContractClient};
-use soroban_sdk::{testutils::Address as _, Address, Env, IntoVal, Symbol, Vec};
+use crate::{ProposalContract, ProposalContractClient, ProposalState, Status};
+use soroban_sdk::{testutils::Address as _, vec, Address, Env, IntoVal, Symbol, Vec};
 
 #[test]
 fn voters_add_and_retrieve_works() {
@@ -51,7 +51,7 @@ fn ensure_admin_auth_is_checked_adding_voters() {
     let admin = Address::random(&env);
     let voter = Address::random(&env);
     let mut voters = Vec::new(&env);
-    voters.push_back(voter.clone());
+    voters.push_back(voter);
 
     client.init(&admin);
     client.add_voters(&voters);
@@ -59,10 +59,61 @@ fn ensure_admin_auth_is_checked_adding_voters() {
     assert_eq!(
         env.auths(),
         [(
-            admin.clone(),
-            client.address.clone(),
+            admin,
+            client.address,
             Symbol::new(&env, "add_voters"),
             (voters,).into_val(&env)
         )]
     )
+}
+
+#[test]
+fn prd_creation_and_query() {
+    let env = Env::default();
+    env.mock_all_auths();
+
+    let contract_id = env.register_contract(None, ProposalContract);
+    let client = ProposalContractClient::new(&env, &contract_id);
+    let admin = Address::random(&env);
+    client.init(&admin);
+
+    let voter_1 = Address::random(&env);
+    let voter_2 = Address::random(&env);
+    let voters = vec![&env, voter_1, voter_2];
+    client.add_voters(&voters);
+
+    let prd_id = 1001u64;
+    client.create_prd(&prd_id);
+
+    assert_eq!(
+        env.auths(),
+        [(
+            admin,
+            client.address.clone(),
+            Symbol::new(&env, "create_prd"),
+            (1001u64,).into_val(&env)
+        )]
+    );
+
+    let state = client.prd_status(&prd_id);
+
+    assert_eq!(
+        ProposalState {
+            status: Status::OpenVoting,
+            votes: 0,
+        },
+        state
+    );
+}
+
+#[test]
+#[should_panic(expected = "NotAuthorized")]
+fn only_admin_can_create_prds() {
+    let env = Env::default();
+    let contract_id = env.register_contract(None, ProposalContract);
+    let client = ProposalContractClient::new(&env, &contract_id);
+    let admin = Address::random(&env);
+    client.init(&admin);
+    
+    client.create_prd(&1);
 }
